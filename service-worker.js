4012a5fb463df53486a744ea2312419d71d9e543
AKAMiNE <120767476+Akamine20496@users.noreply.github.com>
2024-02-17 14:42:05 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,64 @@
 const CACHE_NAME = 'artifact-rng-cache';
 
 const ASSETS = [
-    "../",
-    "../index.html",
-    "../json/manifest.json",
-    "../css/dialog.css",
-    "../css/modalCustomStat.css",
-    "../css/style.css",
-    "./main.js",
-    "./Stat.js",
-    "./Artifact_Piece.js",
-    "./Artifact_Simulator.js",
-    "./Artifact.js",
-    "./Attribute.js",
-    "./CustomStat.js",
-    "./customStatModel.js",
-    "./Dialog.js",
-    "./jquery-3.7.0.min.js",
-    "../asset/Amber Icon.jpg",
-    "../asset/Amber_Icon192.png",
-    "../asset/Amber_Icon512.png",
+    "/",
+    "/index.html",
+    "/app.js",
+    "./json/manifest.json",
+    "./css/dialog.css",
+    "./css/modalCustomStat.css",
+    "./css/style.css",
+    "./js/main.js",
+    "./js/Stat.js",
+    "./js/Artifact_Piece.js",
+    "./js/Artifact_Simulator.js",
+    "./js/Artifact.js",
+    "./js/Attribute.js",
+    "./js/CustomStat.js",
+    "./js/customStatModel.js",
+    "./js/Dialog.js",
+    "./js/jquery-3.7.0.min.js",
+    "./asset/Amber Icon.jpg",
+    "./asset/Amber_Icon192.png",
+    "./asset/Amber_Icon512.png",
     "https://i.pinimg.com/originals/dd/6a/53/dd6a53af112346d57377e9b4403bdc9e.jpg"
 ];
 
 // install
 self.addEventListener('install', event => {
+    // Extend the lifetime of the event until all promises inside waitUntil resolve
     event.waitUntil((async () => {
+        // Open a new cache storage with the specified CACHE_NAME
         const cache = await caches.open(CACHE_NAME);
-        console.log('Adding cache: ', cache);
+        // Log a message indicating the opening of the cache
+        console.log('Adding cache: ', ASSETS);
+        // Add all specified ASSETS to the cache
         cache.addAll(ASSETS);
-    }));
+    })());
 });
 
 // activate
 self.addEventListener("activate", event => {
+    // Extend the lifetime of the event until all promises inside waitUntil resolve
     event.waitUntil(
+        // Retrieve all cache storage keys
         caches.keys().then(cacheNames => {
+            // Wait until all promises inside Promise.all resolve
             return Promise.all(
-                cacheNames.filter(name => name !== CACHE_NAME).map(name => {
-                    console.log('Deleting old cache:', name);
-                    return caches.delete(name);
+                // Map through each cache storage key
+                cacheNames.map(name => {
+                    // Check if the cache storage key matches the specified CACHE_NAME
+                    if (name === CACHE_NAME) {
+                        // If it matches, log a message indicating deletion of the old cache
+                        console.log('Deleting old cache:', name);
+                        // Delete the cache with the matching name
+                        return caches.delete(name);
+                    }
                 })
             );
         })
     );
+    // Immediately take control of clients
     self.clients.claim();
 });
 
